466a7cd630de94afd392c44ab77b15b805609eda
Dan Brown <ltdbtc4@gmail.com>
2025-01-31 02:54:38 +0000

    wip

--- main.py
+++ main.py
@@ -43,15 +43,16 @@ def retrieve_properties_by_id_and_filter(property_id, filter_params):
             if annual_service_charge is None or ground_rent is None or lease_length is None:
                 return
 
-            if filter_params.max_service_charge > float(annual_service_charge) > 0 \
-                    and filter_params.max_ground_rent > float(ground_rent) > 0 \
+            if filter_params.get_max_service_charge() > float(annual_service_charge) > 0 \
+                    and filter_params.get_max_ground_rent() > float(ground_rent) > 0 \
                     and filter_params.get_min_lease_length() < float(lease_length):
                 print(property_link)
 
 
+# TODO ADD WALKING DISTANCE TO POSTCODE PARAM
 if __name__ == "__main__":
-    search_params = SearchParamsBuilder().set_min_bedrooms(2).set_min_price(150000).set_max_price(190000).build()
-    filter_params = FilterParamsBuilder().set_max_ground_rent(200).set_max_service_charge(1600).set_min_lease_length(
+    search_params = SearchParamsBuilder().set_min_bedrooms(2).set_min_price(150000).set_max_price(200000).build()
+    filter_params = FilterParamsBuilder().set_max_ground_rent(300).set_max_service_charge(1800).set_min_lease_length(
         100).set_forbidden_words(["investors only", "cash only", "auction", "tenanted"]).build()
     list(map(lambda property_data: retrieve_properties_by_id_and_filter(property_data["id"], filter_params),
              get_properties(search_params.get_url())))
